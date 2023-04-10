8a45e20e982c3003191ffd9b1b8c4e93816d1e29
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-10 11:55:28 +0200

    Terminated branch

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -41,7 +41,10 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        jdbcTemplate.update("UPDATE author set first_name = ?, last_name = ? where id = ?",
+                author.getFirstName(), author.getLastName(), author.getId());
+
+        return this.getById(author.getId());
     }
 
     @Override
--- src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
@@ -1,8 +1,6 @@
 package guru.springframework.jdbc;
 
 import guru.springframework.jdbc.dao.AuthorDao;
-
-import guru.springframework.jdbc.dao.AuthorDaoImpl;
 import guru.springframework.jdbc.domain.Author;
 
 import org.junit.jupiter.api.Test;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -22,7 +19,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(AuthorDaoImpl.class)
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
 public class DaoIntegrationTest {
 
     @Autowired
@@ -65,9 +62,8 @@ public class DaoIntegrationTest {
 
         Author saved = authorDao.saveNewAuthor(author);
 
-        System.out.println("New Id is: " + saved.getId());
-
         assertThat(saved).isNotNull();
+        assertThat(saved.getId()).isGreaterThan(3L);
     }
 
     @Test
@@ -75,13 +71,16 @@ public class DaoIntegrationTest {
         Author author = authorDao.findAuthorByName("Craig", "Walls");
 
         assertThat(author).isNotNull();
+        assertThat(author.getId()).isEqualTo(1L);
     }
 
     @Test
     void testGetAuthor() {
 
-        Author author = authorDao.getById(1l);
+        Author author = authorDao.getById(1L);
 
-        assertThat(author.getId()).isNotNull();
+        assertThat(author).isNotNull();
+        assertThat(author.getFirstName()).isEqualTo("Craig");
+        assertThat(author.getLastName()).isEqualTo("Walls");
     }
 }
